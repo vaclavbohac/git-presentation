cc0287d48e46cad6469f6d4e337ef29c6c0e6399
Vaclav Bohac <bohac.v@gmail.com>
2013-06-26 17:05:24 +0200

    Init.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+/*jslint browser:true*/
+
+/*global jQuery*/
+
+(function ($) {
+    "use strict";
+
+    var popup;
+
+    $(document).ready(function () {
+        var $controlPanel = $('#control-panel'),
+
+            $controls = $controlPanel.find('.btn');
+
+        $('#engine-starter').click(function () {
+            popup = window.open('examples.html', 'Školení GIT - examples');
+            popup.onbeforeunload = function () {
+                $('#engine-starter').show();
+
+                $('#control-panel').addClass('hidden');
+            };
+
+            $('#control-panel').removeClass('hidden');
+
+            $(this).hide();
+        });
+
+        $('#pop-up-control-init').click(function () {
+            $('.example', popup.document).hide();
+
+            $('#git-init', popup.document).show();
+        });
+
+        $controlPanel.on('click', '.btn', function () {
+            var $elem = $(this);
+
+            if ($elem.is('.active')) {
+                $elem.removeClass('active');
+            } else {
+                $elem.addClass('active');
+            }
+        });
+
+        $controlPanel.on('click', '.btn', function () {
+            var target = $(this).data('example'),
+
+                slide = popup.document.getElementById(target);
+
+            if ($(this).is('.active')) {
+                $(slide).addClass('active');
+            } else {
+                $(slide).removeClass('active');
+            }
+        });
+    });
+}(jQuery));
